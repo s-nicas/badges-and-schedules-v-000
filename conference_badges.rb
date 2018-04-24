09ecd531b7a654054ecbe1fa0b011cc498b5ad0b
Sonia  Nicas <sonia.d.nicas@gmail.com>
2018-04-24 03:33:44 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,5 @@
 def badge_maker(name)
- puts "Hello, my name is #{name}." 
+ return "Hello, my name is #{name}." 
 end 
 
 def batch_badge_creator(speakers_array)
@@ -8,7 +8,7 @@ end
 
 def assign_rooms(speakers_array)
   speakers_array.each_with_index.collect {|name, index| 
-   puts  "Hello, #{name}! You'll be assigned to room #{index+1}!"}
+    "Hello, #{name}! You'll be assigned to room #{index+1}!"}
 end 
 
 def printer(speakers_array)
