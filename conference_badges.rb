caa5e5e8888493bce700e9c11261016a75e7cffe
Sonia  Nicas <sonia.d.nicas@gmail.com>
2018-04-24 03:08:40 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,8 @@
 def badge_maker(name)
  return "Hello, my name is #{name}." 
 end 
+
+def batch_badge_creator(speakers_array)
+  speakers_array.collect{|name| "Hello, my name is #{name}}
+    
+end 
